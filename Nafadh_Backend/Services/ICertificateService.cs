e0788e443e0374a2e2d486dc8bc538625a56586d
KarimSalaheldeen <eng.karimsalaheldeen@gmail.com>
2026-08-04 15:44:27 +0400

    test case on IRepos and IServices

--- Nafadh_Backend/Services/ICertificateService.cs
+++ Nafadh_Backend/Services/ICertificateService.cs
@@ -10,5 +10,9 @@ namespace Nafadh_Backend.Services
     public interface ICertificateService
     {
         // TODO: define business-logic contract methods for this entity
+        Task AddCertificateAsync(NFD_Certificate certificate);
+        Task<NFD_Certificate?> GetCertificateByIdAsync(int certificateId);
+        Task UpdateCertificateAsync(NFD_Certificate certificate);
+        Task DeleteCertificateAsync(NFD_Certificate certificate);
     }
 }
