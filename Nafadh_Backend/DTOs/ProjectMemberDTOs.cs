1b1395131e0b7b4284711e3ab38884949ad075f2
moza-stack <moza.m@gmail.com>
2026-08-05 14:53:58 +0400

    ProjectMemberDTOs

--- Nafadh_Backend/DTOs/ProjectMemberDTOs.cs
+++ Nafadh_Backend/DTOs/ProjectMemberDTOs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nafadh_Backend.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +8,28 @@ namespace Nafadh_Backend.DTOs
     public class ProjectMemberDTOs
     {
 
+        public int MemberId { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public int TraineeId { get; set; }
+
+        public NFD_ProjectMemberRole Role { get; set; }
+    }
+
+    public class CreateProjectMemberDTO
+    {
+        public int ProjectId { get; set; }
+
+        public int TraineeId { get; set; }
+
+        public NFD_ProjectMemberRole Role { get; set; }
+    }
+
+    public class UpdateProjectMemberDTO
+    {
+        public NFD_ProjectMemberRole Role { get; set; }
+    
+    
     }
 }
