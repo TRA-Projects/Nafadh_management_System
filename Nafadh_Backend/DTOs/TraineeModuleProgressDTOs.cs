19b4e80ef140f592f295d43e160bfcb5bb2f0a44
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-08-05 11:01:05 +0400

    create Trainee Module Progress DTO

---
+++ Nafadh_Backend/DTOs/TraineeModuleProgressDTOs.cs
@@ -0,0 +1,37 @@
+﻿using Nafadh_Backend.Enums;
+namespace Nafadh_Backend.DTOs
+{
+    public class CompleteModuleDto
+    {
+        public int TraineeId { get; set; }
+
+        public int ModuleId { get; set; }
+    }
+
+    public class UpdateTraineeModuleProgressDto
+    {
+        public NFD_ModuleProgressStatus Status { get; set; }
+
+        public DateTime? CompletedAt { get; set; }
+    }
+
+    public class TraineeModuleProgressDto
+    {
+        public int ProgressId { get; set; }
+
+        public int TraineeId { get; set; }
+
+        public int ModuleId { get; set; }
+
+        public NFD_ModuleProgressStatus Status { get; set; }
+
+        public DateTime? CompletedAt { get; set; }
+    }
+
+    public class TraineeProgressPercentageDto
+    {
+        public int TraineeId { get; set; }
+
+        public double Percentage { get; set; }
+    }
+}
