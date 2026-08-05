e17eb294884847b3300f8c5b455ebf86d08c4d90
Ebtesam <ebtesam>
2026-08-05 11:57:07 +0400

    Add NFD_CompanyBranchDTO.cs

---
+++ Nafadh_Backend/DTOs/NFD_CompanyBranchDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnFromHome.DTOs
+{
+    // Input DTO
+    public class NFD_CompanyBranchInputDTO
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Location { get; set; } = string.Empty;
+
+        [MaxLength(150)]
+        public string? ContactPoint { get; set; }
+
+        [Required]
+        public int CompanyId { get; set; }
+    }
+
+
+    // Output DTO
+    public class NFD_CompanyBranchOutputDTO
+    {
+        public int BranchId { get; set; }
+
+        public string Location { get; set; } = string.Empty;
+
+        public string? ContactPoint { get; set; }
+
+        public int CompanyId { get; set; }
+    }
+}
