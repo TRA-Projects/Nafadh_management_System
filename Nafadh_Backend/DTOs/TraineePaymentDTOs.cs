54afc3cf5926180722a292c61e4f837297eed48d
“Anoud <anoudalsaidi1993@gmail.com>
2026-08-05 11:52:32 +0400

    Add Trainee Payment DTOs

---
+++ Nafadh_Backend/DTOs/TraineePaymentDTOs.cs
@@ -0,0 +1,16 @@
+﻿namespace Nafadh_Backend.DTOs
+{
+    public class TraineePaymentDTOs
+    {
+        public int TraineePaymentId { get; set; }
+        public int EnrollmentId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+
+    public class CreateTraineePaymentDTO
+    {
+        public int EnrollmentId { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
