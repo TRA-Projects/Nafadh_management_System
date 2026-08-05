e7edd268a8819818534dc65905636d1e236ccd36
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-08-05 10:45:10 +0400

    Create Training MaterialDto

---
+++ Nafadh_Backend/DTOs/TrainingMaterialDTOs.cs
@@ -0,0 +1,36 @@
+﻿using Nafadh_Backend.Enums;
+namespace Nafadh_Backend.DTOs
+{
+    public class CreateTrainingMaterialDto
+    {
+        public string FileUrl { get; set; } = string.Empty;
+
+        public NFD_FileType FileType { get; set; }
+
+        public int LessonId { get; set; }
+
+        public int UploadedByUserId { get; set; }
+    }
+
+    public class UpdateTrainingMaterialDto
+    {
+        public string FileUrl { get; set; } = string.Empty;
+
+        public NFD_FileType FileType { get; set; }
+    }
+
+    public class TrainingMaterialDto
+    {
+        public int MaterialId { get; set; }
+
+        public string FileUrl { get; set; } = string.Empty;
+
+        public NFD_FileType FileType { get; set; }
+
+        public DateTime UploadDate { get; set; }
+
+        public int LessonId { get; set; }
+
+        public int UploadedByUserId { get; set; }
+    }
+}
