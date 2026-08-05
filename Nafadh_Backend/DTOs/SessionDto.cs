869f8bd776f9b879a13c0ed447339d286463df1a
Malakaka <malakalsenine@gmail.com>
2026-08-05 15:30:49 +0400

    add Session DTOs for creation, updates, and status management

---
+++ Nafadh_Backend/DTOs/SessionDto.cs
@@ -0,0 +1,48 @@
+﻿using Nafadh_Backend.Enums;
+
+namespace Nafadh_Backend.DTOs
+{
+    public class SessionDto
+    {
+        public int SessionId { get; set; }
+        public int BatchId { get; set; }
+        public int TrainerId { get; set; }
+        public DateTime SessionDate { get; set; }
+        public string? StartTime { get; set; }
+        public string? EndTime { get; set; }
+        public string? MeetingLink { get; set; }
+        public string? Topic { get; set; }
+        public string? LearningObjectives { get; set; }
+        public string? RecordingUrl { get; set; }
+        public string? Summary { get; set; }
+        public NFD_SessionStatus Status { get; set; }
+
+    }
+
+    public class CreateSessionDto
+    {
+        public int BatchId { get; set; }
+        public int TrainerId { get; set; }
+        public DateTime SessionDate { get; set; }
+        public string? StartTime { get; set; }
+        public string? EndTime { get; set; }
+        public string? MeetingLink { get; set; }
+        public string? Topic { get; set; }
+        public string? LearningObjectives { get; set; }
+    }
+
+    public class UpdateSessionDto
+    {
+        public string? Topic { get; set; }
+        public string? MeetingLink { get; set; }
+        public string? RecordingUrl { get; set; }
+        public string? LearningObjectives { get; set; }
+        public string? Summary { get; set; }
+    }
+
+    public class UpdateSessionStatusDto
+    {
+        public NFD_SessionStatus Status { get; set; }
+    }
+
+}
