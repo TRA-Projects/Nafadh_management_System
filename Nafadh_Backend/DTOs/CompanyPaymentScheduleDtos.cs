81c68770ea472c3d7f89d4dcb815d9590bffb569
“Anoud <anoudalsaidi1993@gmail.com>
2026-08-06 09:10:49 +0400

    Add Company Payment Schedule Dto

---
+++ Nafadh_Backend/DTOs/CompanyPaymentScheduleDtos.cs
@@ -0,0 +1,41 @@
+﻿using Nafadh_Backend.Enums;
+
+namespace Nafadh_Backend.DTOs
+{
+    public class CreateCompanyPaymentScheduleDto
+    {
+        public int CompanyPaymentId { get; set; }
+
+        public int NumberOfMonths { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+
+
+    public class MarkCompanyPaymentSchedulePaidDto
+    {
+        public DateTime PaidDate { get; set; }
+    }
+
+
+    public class CompanyPaymentScheduleResponseDto
+    {
+        public int ScheduleId { get; set; }
+
+        public int MonthNumber { get; set; }
+
+        public string? MonthLabel { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public NFD_PaymentScheduleStatus Status { get; set; }
+
+        public DateTime? PaidDate { get; set; }
+
+        public int CompanyPaymentId { get; set; }
+    }
+}
