c062948b0a66d368ee06fbd8ba080aff2c4bcb5b
“Noura” <noura.almaskari11@gmail.com>
2026-08-06 10:36:43 +0400

    edit swagger configs

--- Nafadh_Backend/Program.cs
+++ Nafadh_Backend/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
 using Nafadh_Backend.Filters;
 using Nafadh_Backend.Repositories;
 using Nafadh_Backend.Services;
@@ -180,6 +182,39 @@ namespace Nafadh_Backend
             builder.Services.AddScoped<ICompanyPaymentService, CompanyPaymentService>();
             builder.Services.AddScoped<ICompanyPaymentScheduleRepository, CompanyPaymentScheduleRepository>();
             builder.Services.AddScoped<ICompanyPaymentScheduleService, CompanyPaymentScheduleService>();
+
+            // ── Swagger with JWT support ───────────────────────────────────────
+            builder.Services.AddEndpointsApiExplorer();
+            //  builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter your JWT token in the box below"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id   = "Bearer"
+                }
+            },
+            new List<string>()
+        }
+    });
+            });
 
             var app = builder.Build();
 
