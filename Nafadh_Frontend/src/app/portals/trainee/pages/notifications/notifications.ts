2954bec74c8fd72d1610316e0cfd5a0e0af2b125
malak aloufi <mkmalaufi@gmail.com>
2026-08-18 15:20:09 +0400

    Update trainee notifications page

--- Nafadh_Frontend/src/app/portals/trainee/pages/notifications/notifications.ts
+++ Nafadh_Frontend/src/app/portals/trainee/pages/notifications/notifications.ts
@@ -10,26 +10,48 @@ import { NotificationDto, WarningDto } from '../../../../core/models/dtos';
   templateUrl: './notifications.html',
 })
 export class TraineeNotifications implements OnInit {
+
   enrollmentId = 1;
+
   notifications = signal<NotificationDto[]>([]);
   warnings = signal<WarningDto[]>([]);
+
   filter = signal<'all' | 'unread'>('all');
 
-  constructor(private api: TraineeApi, private auth: AuthService) {}
+  constructor(
+    private api: TraineeApi,
+    private auth: AuthService
+  ) {}
+
   ngOnInit() {
     const uid = this.auth.userId ?? 4;
-    this.api.getNotifications(uid).subscribe((d) => this.notifications.set(d ?? []));
-    this.api.getMyWarnings(this.enrollmentId).subscribe((d) => this.warnings.set(d ?? []));
+
+    this.api.getNotifications(uid).subscribe((d) => {
+      this.notifications.set(d ?? []);
+    });
+
+    this.api.getMyWarnings(this.enrollmentId).subscribe((d) => {
+      this.warnings.set(d ?? []);
+    });
   }
 
   filtered() {
-    if (this.filter() === 'unread') return this.notifications().filter((n) => !n.isRead);
+    if (this.filter() === 'unread') {
+      return this.notifications().filter((n) => !n.isRead);
+    }
+
     return this.notifications();
   }
 
   markRead(n: NotificationDto) {
     this.api.markRead(n.notificationId).subscribe(() => {
-      this.notifications.update((list) => list.map((x) => (x.notificationId === n.notificationId ? { ...x, isRead: true } : x)));
+      this.notifications.update((list) =>
+        list.map((x) =>
+          x.notificationId === n.notificationId
+            ? { ...x, isRead: true }
+            : x
+        )
+      );
     });
   }
 }
