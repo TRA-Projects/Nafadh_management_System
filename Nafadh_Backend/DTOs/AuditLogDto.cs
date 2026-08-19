f3611a77d325a38a4ec256d80bf9768cb4de4830
Shima Al Sharqi <Shimaalsharqi@gmail.com>
2026-08-19 15:42:31 +0400

    Add Auditlog DTO

---
+++ Nafadh_Backend/DTOs/AuditLogDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Nafadh_Backend.DTOs
+{
+    public class AuditLogDto
+    {
+        public int LogId { get; set; }
+        public string Action { get; set; } = string.Empty;
+        public int? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string EntityName { get; set; } = string.Empty;
+        public int? EntityId { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
