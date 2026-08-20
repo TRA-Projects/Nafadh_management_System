f79f1e41b9f926f2a298f3bd6717be6c64f1134a
“Anoud <anoudalsaidi1993@gmail.com>
2026-08-20 11:53:09 +0400

    update trainee program flow and resolve program ID routing

--- Nafadh_Frontend/src/app/portals/trainee/trainee.routes.ts
+++ Nafadh_Frontend/src/app/portals/trainee/trainee.routes.ts
@@ -11,6 +11,7 @@ export const TRAINEE_ROUTES: Routes = [
       { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard').then((m) => m.TraineeDashboard) },
       { path: 'profile', loadComponent: () => import('./pages/profile/profile').then((m) => m.TraineeProfile) },
       { path: 'program', loadComponent: () => import('./pages/program/program').then((m) => m.TraineeProgram) },
+    // {path: 'program/:id',loadComponent: () =>import('./pages/program/program').then((m) => m.TraineeProgram)},
       { path: 'tasks', loadComponent: () => import('./pages/tasks/tasks').then((m) => m.TraineeTasks) },
       { path: 'attendance', loadComponent: () => import('./pages/attendance/attendance').then((m) => m.TraineeAttendance) },
       { path: 'notifications', loadComponent: () => import('./pages/notifications/notifications').then((m) => m.TraineeNotifications) },
